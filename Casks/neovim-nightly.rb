71caed44b096bf71dc7b7e00e31eb3426e067243
Bellisario <72039923+Bellisario@users.noreply.github.com>
2025-10-10 06:14:50 +0000

    chore: update formula

--- Casks/neovim-nightly.rb
+++ Casks/neovim-nightly.rb
@@ -1,14 +1,14 @@
 cask "neovim-nightly" do
-  version "nightly-645206e87f64de37180800466fdacee7228c9913"
+  version "nightly-56713ef8724b7778e41fc3cfa46bb6e04c661fa6"
 
   on_arm do
-    sha256 "85cd7fa38395d7158a0106d37e6bf28fd30273e86cd1af4ed9c6f71adf2ff537"
+    sha256 "c8dc585f5d4a0d1c7d7f8eb3150d16b7cb3be263fcb169eca8a7514b4b352a2d"
     url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-arm64.tar.gz",
         verified: "github.com/neovim"
     binary "nvim-macos-arm64/bin/nvim"
   end
   on_intel do
-    sha256 "41c910fec073ec2790f74026951a36ba4bcc32fcf511428a92765b05a579ad80"
+    sha256 "88ee43df499281f5a588b80c7c76a45dc6065d07fcee72f0f2a589ca83cf04ac"
     url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-x86_64.tar.gz",
         verified: "github.com/neovim"
     binary "nvim-macos-x86_64/bin/nvim"
